2c039108e8ec848b666798613d6e26c1339d6c61
Vekhov Vadim <106478770+wizardVadim@users.noreply.github.com>
2026-02-14 12:20:58 +0800

    final lighting

--- ConsoleApplicationOpenGL/ConsoleApplicationOpenGL.cpp
+++ ConsoleApplicationOpenGL/ConsoleApplicationOpenGL.cpp
@@ -24,6 +24,10 @@ glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
 glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
 const float cameraSpeed = 0.001f;
 
+float const LIGHT_RED = 1.0f;
+float const LIGHT_GREEN = 1.0f;
+float const LIGHT_BLUE = 1.0f;
+
 void mouse_callback(GLFWwindow* window, double xposIn, double yposIn);
 
 int main()
@@ -51,21 +55,50 @@ int main()
     }
     glEnable(GL_DEPTH_TEST);
 
-    Shader shader;
-    shader.loadFromFiles("shaders/vertex.txt", "shaders/fragment.txt");
-    shader.use();
-
-    Model loadedModel("models/lab3.obj");
-
     glm::mat4 projection = glm::perspective(
         glm::radians(45.0f),
         (float)SCR_WIDTH / (float)SCR_HEIGHT,
         0.1f,
         100.0f
     );
+
+    Shader shader;
+    shader.loadFromFiles("shaders/vertex.txt", "shaders/fragment.txt");
+    shader.use();
     shader.setUniformMatrix4fv("projection", glm::value_ptr(projection));
 
-    glm::mat4 modelMatrix = glm::mat4(1.0f);
+    shader.setUniform3f("material.ambient", 0.282f, 0.387f, 0.277f);
+    shader.setUniform3f("material.diffuse", 0.4f, 0.6f, 0.4f);    
+    shader.setUniform3f("material.specular", 0.5f, 0.5f, 0.5f);   
+    shader.setUniform1f("material.shininess", 32.0f);
+
+    shader.setUniform3f("light.position", 2.0f, 5.0f, 3.0f);       
+    shader.setUniform3f("light.ambient", 0.1f, 0.1f, 0.1f);        
+    shader.setUniform3f("light.diffuse", 1.0f, 1.0f, 1.0f);        
+    shader.setUniform3f("light.specular", 1.0f, 1.0f, 1.0f);
+
+    Shader shader2;
+    shader2.loadFromFiles("shaders/vertex.txt", "shaders/fragment.txt");
+    shader2.use();
+    shader2.setUniformMatrix4fv("projection", glm::value_ptr(projection));
+
+    shader2.setUniform3f("material.ambient", 0.108f, 0.153f, 0.124f);
+    shader2.setUniform3f("material.diffuse", 0.2f, 0.3f, 0.25f);
+    shader2.setUniform3f("material.specular", 0.3f, 0.3f, 0.3f);
+    shader2.setUniform1f("material.shininess", 16.0f);
+
+    shader2.setUniform3f("light.position", 2.0f, 5.0f, 3.0f);
+    shader2.setUniform3f("light.ambient", 0.1f, 0.1f, 0.1f);
+    shader2.setUniform3f("light.diffuse", 1.0f, 1.0f, 1.0f);
+    shader2.setUniform3f("light.specular", 1.0f, 1.0f, 1.0f);
+
+    Model loadedModel("models/lab3.obj");
+    Model fieldModel("models/field.obj");
+
+    glm::mat4 modelMatrixLab = glm::mat4(1.0f);
+    glm::mat4 modelMatrixField = glm::mat4(1.0f);
+    modelMatrixField = glm::translate(modelMatrixField, glm::vec3(0.0f, -0.03f, 0.0f));
+
 
     while (!glfwWindowShouldClose(window))
     {
@@ -81,21 +114,25 @@ int main()
             glfwTerminate();
 
         glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
-        shader.setUniformMatrix4fv("view", glm::value_ptr(view));
-        shader.setUniformMatrix4fv("projection", glm::value_ptr(projection));
-        shader.setUniformMatrix4fv("model", glm::value_ptr(modelMatrix));
-        float timeValue = glfwGetTime();
-        float redValue = (sin(timeValue) / 2.0f) + 0.5f;
-        float greenValue = (cos(timeValue) / 2.0f) + 0.5f;
-        shader.setUniform3f("lightColor", 1.0f, 1.0f, 1.0f);
-        shader.setUniform3f("objectColor", redValue, greenValue, 0.5f);
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         shader.use();
-
+        shader.setUniformMatrix4fv("view", glm::value_ptr(view));
+        shader.setUniformMatrix4fv("projection", glm::value_ptr(projection));
+        shader.setUniformMatrix4fv("model", glm::value_ptr(modelMatrixLab));
+        /*shader.setUniform3f("lightColor", LIGHT_RED, LIGHT_GREEN, LIGHT_BLUE);
+        shader.setUniform3f("objectColor", 0.282f, 0.387f, 0.277f);*/
         loadedModel.Draw(shader);
+
+        shader2.use();
+        shader2.setUniformMatrix4fv("view", glm::value_ptr(view));
+        shader2.setUniformMatrix4fv("projection", glm::value_ptr(projection));
+        shader2.setUniformMatrix4fv("model", glm::value_ptr(modelMatrixField));
+        /*shader2.setUniform3f("lightColor", LIGHT_RED, LIGHT_GREEN, LIGHT_BLUE);
+        shader2.setUniform3f("objectColor", 0.108f, 0.153f, 0.124f);*/
+        fieldModel.Draw(shader2);
 
         glfwSwapBuffers(window);
         glfwPollEvents();

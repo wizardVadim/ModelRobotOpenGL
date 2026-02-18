3fa75b88b3a1fa04997ad66bb556df21ec029936
Vekhov Vadim <106478770+wizardVadim@users.noreply.github.com>
2026-02-18 12:49:44 +0800

    final commit

--- ConsoleApplicationOpenGL/ConsoleApplicationOpenGL.cpp
+++ ConsoleApplicationOpenGL/ConsoleApplicationOpenGL.cpp
@@ -19,7 +19,7 @@ float yaw = -90.0f;
 float pitch = 0.0f;
 float lastX = SCR_WIDTH / 2, lastY = SCR_HEIGHT / 2;
 bool firstMouse = true;
-glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraPos = glm::vec3(2.0f, 1.0f, 3.0f);
 glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
 glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
 const float cameraSpeed = 0.001f;
@@ -29,7 +29,30 @@ float const LIGHT_GREEN = 1.0f;
 float const LIGHT_BLUE = 1.0f;
 
 int currentArm = 1;
-const float detailsSpeed = 0.0001f;
+const float detailsSpeed = 0.0005f;
+
+float minPosRollerZ = 0.0f;
+float maxPosRollerZ = 4.02f;
+float currentPosRollerZ = 0.0f;
+
+float maxPosArmsY = 0.0f;
+float minPosArmsY = -0.48f;
+float currentPosArm1Y = 0.0f;
+float currentPosArm2Y = 0.0f;
+
+float minAngleRotate = 0.0f;
+float maxAngleRotate = 45.0f;
+float xPivot = -0.461076f;
+float yPivot = -1.35491f;
+float rotateSpeed = 0.01f;
+
+glm::vec3 vecRotate = glm::vec3(0.0f, 0.0f, 1.0f);
+glm::vec3 pivot = glm::vec3(xPivot, yPivot, 0.0f);
+
+float yArm1Pivot = yPivot;
+float yArm2Pivot = yPivot;
+glm::vec3 arm1pivot = glm::vec3(xPivot, yArm1Pivot, 0.0f);
+glm::vec3 arm2pivot = glm::vec3(xPivot, yArm2Pivot, 0.0f);
 
 void mouse_callback(GLFWwindow* window, double xposIn, double yposIn);
 Shader getArm1Shader(glm::mat4 projection);
@@ -95,9 +118,19 @@ int main()
     modelMatrixField = glm::translate(modelMatrixField, glm::vec3(0.0f, -0.03f, 0.0f));
 
     bool armChangedThisFrame = false;
+    float currentAngleRotate = 0.0f;
+
+    ////debug
+    //glm::vec3 debugPivot = glm::vec3(0.0f, 0.0f, 0.0f);
+    //glm::mat4 initialMatrixArm1;  // сохраняем начальное положение
+    //glm::mat4 initialMatrixArm2;
+    //glm::mat4 initialMatrixRotateDetail;
+    //bool isRotating = false;
+
 
     while (!glfwWindowShouldClose(window))
     {
+
         if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
             cameraPos += cameraSpeed * cameraFront;
         if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
@@ -118,58 +151,100 @@ int main()
 
         if (glfwGetKey(window, GLFW_KEY_PAGE_UP) == GLFW_PRESS) {
             glm::vec3 vecTranslate = glm::vec3(0.0f, detailsSpeed, 0.0f);
-            if (currentArm == 1)
-                if (getPositionFromMatrix(modelMatrixArm1).y < 0.0f)
+            if (currentArm == 1) {
+                if (currentPosArm1Y < maxPosArmsY) {
                     modelMatrixArm1 = glm::translate(modelMatrixArm1, vecTranslate);
-            else
-                if (getPositionFromMatrix(modelMatrixArm2).y < 0.0f)
+                    yArm1Pivot += detailsSpeed;
+                    currentPosArm1Y += detailsSpeed;
+                    arm1pivot = glm::vec3(xPivot, yArm1Pivot, 0.0f);
+                }
+            
+            }
+            else if (currentArm == 2) {
+                if (currentPosArm2Y < maxPosArmsY)
                     modelMatrixArm2 = glm::translate(modelMatrixArm2, vecTranslate);
+                yArm2Pivot += detailsSpeed;
+                currentPosArm2Y += detailsSpeed;
+                arm2pivot = glm::vec3(xPivot, yArm2Pivot, 0.0f);
+            }
         }
         if (glfwGetKey(window, GLFW_KEY_PAGE_DOWN) == GLFW_PRESS) {
             glm::vec3 vecTranslate = glm::vec3(0.0f, -detailsSpeed, 0.0f);
-            if (currentArm == 1)
-                modelMatrixArm1 = glm::translate(modelMatrixArm1, vecTranslate);
-            else
-                modelMatrixArm2 = glm::translate(modelMatrixArm2, vecTranslate);
+            if (currentArm == 1) {
+                if (currentPosArm1Y > minPosArmsY) {
+                    modelMatrixArm1 = glm::translate(modelMatrixArm1, vecTranslate);
+                    yArm1Pivot -= detailsSpeed;
+                    currentPosArm1Y -= detailsSpeed;
+                    arm1pivot = glm::vec3(xPivot, yArm1Pivot, 0.0f);
+                }
+            } else if (currentArm == 2) {
+                if (currentPosArm2Y > minPosArmsY) {
+                    modelMatrixArm2 = glm::translate(modelMatrixArm2, vecTranslate);
+                    yArm2Pivot -= detailsSpeed;
+                    currentPosArm2Y -= detailsSpeed;
+                    arm2pivot = glm::vec3(xPivot, yArm2Pivot, 0.0f);
+                }
+            }
         }
 
         if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS) {
-            glm::vec3 vecTranslate = glm::vec3(0.0f, 0.0f, detailsSpeed);
-            modelMatrixRoller = glm::translate(modelMatrixRoller, vecTranslate);
-            modelMatrixArm1 = glm::translate(modelMatrixArm1, vecTranslate);
-            modelMatrixArm2 = glm::translate(modelMatrixArm2, vecTranslate);
-            modelMatrixRotateDetail = glm::translate(modelMatrixRotateDetail, vecTranslate);
+            if (currentPosRollerZ < maxPosRollerZ) {
+                glm::vec3 vecTranslate = glm::vec3(0.0f, 0.0f, detailsSpeed);
+                modelMatrixRoller = glm::translate(modelMatrixRoller, vecTranslate);
+                modelMatrixArm1 = glm::translate(modelMatrixArm1, vecTranslate);
+                modelMatrixArm2 = glm::translate(modelMatrixArm2, vecTranslate);
+                modelMatrixRotateDetail = glm::translate(modelMatrixRotateDetail, vecTranslate);
+
+                currentPosRollerZ += detailsSpeed;
+            }
         }
 
         if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS) {
-            glm::vec3 vecTranslate = glm::vec3(0.0f, 0.0f, -detailsSpeed);
-            modelMatrixRoller = glm::translate(modelMatrixRoller, vecTranslate);
-            modelMatrixArm1 = glm::translate(modelMatrixArm1, vecTranslate);
-            modelMatrixArm2 = glm::translate(modelMatrixArm2, vecTranslate);
-            modelMatrixRotateDetail = glm::translate(modelMatrixRotateDetail, vecTranslate);
+            if (currentPosRollerZ > minPosRollerZ) {
+                glm::vec3 vecTranslate = glm::vec3(0.0f, 0.0f, -detailsSpeed);
+                modelMatrixRoller = glm::translate(modelMatrixRoller, vecTranslate);
+                modelMatrixArm1 = glm::translate(modelMatrixArm1, vecTranslate);
+                modelMatrixArm2 = glm::translate(modelMatrixArm2, vecTranslate);
+                modelMatrixRotateDetail = glm::translate(modelMatrixRotateDetail, vecTranslate);
+
+                currentPosRollerZ -= detailsSpeed;
+            }
         }
 
         if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
-            glm::vec3 vecRotate = glm::vec3(0.0f, 0.0f, 1.0f);
-            glm::vec3 armCenter = glm::vec3(0.0f, 0.0003f, 0.0f);
-            modelMatrixArm1 = glm::translate(modelMatrixArm1, armCenter);
-            modelMatrixArm1 = glm::rotate(modelMatrixArm1, glm::radians(0.01f), vecRotate);
-            modelMatrixArm2 = glm::translate(modelMatrixArm2, armCenter);
-            modelMatrixArm2 = glm::rotate(modelMatrixArm2, glm::radians(0.01f), vecRotate);
-            /*modelMatrixArm2 = glm::rotate(modelMatrixArm2, glm::radians(0.01f), vecRotate);
-            modelMatrixRotateDetail = glm::rotate(modelMatrixRotateDetail, glm::radians(0.01f), vecRotate);*/
+            if (currentAngleRotate > minAngleRotate) {
+                modelMatrixArm1 = glm::translate(modelMatrixArm1, -arm1pivot);
+                modelMatrixArm1 = glm::rotate(modelMatrixArm1, glm::radians(rotateSpeed), vecRotate);
+                modelMatrixArm1 = glm::translate(modelMatrixArm1, arm1pivot);
+
+                modelMatrixArm2 = glm::translate(modelMatrixArm2, -arm2pivot);
+                modelMatrixArm2 = glm::rotate(modelMatrixArm2, glm::radians(rotateSpeed), vecRotate);
+                modelMatrixArm2 = glm::translate(modelMatrixArm2, arm2pivot);
+
+                modelMatrixRotateDetail = glm::translate(modelMatrixRotateDetail, -pivot);
+                modelMatrixRotateDetail = glm::rotate(modelMatrixRotateDetail, glm::radians(rotateSpeed), vecRotate);
+                modelMatrixRotateDetail = glm::translate(modelMatrixRotateDetail, pivot);
+
+                currentAngleRotate -= rotateSpeed;
+            }
         }
 
         if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
-            glm::vec3 vecRotate = glm::vec3(0.0f, 0.0f, 1.0f);
-            glm::vec3 armCenter = glm::vec3(0.0f, 0.0003f, 0.0f);
-            modelMatrixArm1 = glm::translate(modelMatrixArm1, -armCenter);
-            modelMatrixArm1 = glm::rotate(modelMatrixArm1, glm::radians(-0.01f), vecRotate);
-            modelMatrixArm2 = glm::translate(modelMatrixArm2, -armCenter);
-            modelMatrixArm2 = glm::rotate(modelMatrixArm2, glm::radians(-0.01f), vecRotate);
-            /*modelMatrixArm1 = glm::rotate(modelMatrixArm1, glm::radians(-0.01f), vecRotate);
-            modelMatrixArm2 = glm::rotate(modelMatrixArm2, glm::radians(-0.01f), vecRotate);
-            modelMatrixRotateDetail = glm::rotate(modelMatrixRotateDetail, glm::radians(-0.01f), vecRotate);*/
+            if (currentAngleRotate < maxAngleRotate) {
+                modelMatrixArm1 = glm::translate(modelMatrixArm1, -arm1pivot);
+                modelMatrixArm1 = glm::rotate(modelMatrixArm1, glm::radians(-rotateSpeed), vecRotate);
+                modelMatrixArm1 = glm::translate(modelMatrixArm1, arm1pivot);
+                
+                modelMatrixArm2 = glm::translate(modelMatrixArm2, -arm2pivot);
+                modelMatrixArm2 = glm::rotate(modelMatrixArm2, glm::radians(-rotateSpeed), vecRotate);
+                modelMatrixArm2 = glm::translate(modelMatrixArm2, arm2pivot);
+
+                modelMatrixRotateDetail = glm::translate(modelMatrixRotateDetail, -pivot);
+                modelMatrixRotateDetail = glm::rotate(modelMatrixRotateDetail, glm::radians(-rotateSpeed), vecRotate);
+                modelMatrixRotateDetail = glm::translate(modelMatrixRotateDetail, pivot);
+
+                currentAngleRotate += rotateSpeed;
+            }
         }
 
         glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
